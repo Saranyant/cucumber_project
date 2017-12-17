0aaf97050f2ad449bc35672bfd63a1c539f53fe6
unknown <saranyant@MAVRLT205.Mav.Est>
2017-12-17 16:49:41 +0530

    execute

--- src/test/java/cucumber_project/Runner.java
+++ src/test/java/cucumber_project/Runner.java
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(monochrome=true, 
+@CucumberOptions(monochrome=true,
 features="C:\\Users\\saranyant\\NewCucumber\\cucumber_project", glue="Stepdef",
 format={"pretty","html:reports","json:target/Runcuke/cucumber.json"},tags="@tag1")
 
